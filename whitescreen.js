ab4f116bf94266d6bdf0446203c7570c08647d81
Len <next_100@sina.com>
2018-04-23 23:01:28 +0800

    support offline mode

--- whitescreen.js
+++ whitescreen.js
@@ -8,13 +8,14 @@ if(typeof window.MutationObserver!=='undefined'){
     let rootElement = document.body;
     const WHITE_STATUS_DURATION = 1000;
     const NORMAL_PAGE_MINI_HEIGHT = 100;
-
+    const STORAGE_KEY = 'ERROR_WHITE_SCREEN';
     const WhitescreenDetect = {
       init(root, options){
           this.observer = new MutationObserver(this._changeHandler.bind(this));
           this.root = root;
           this.observer.observe(root, options);
           this._changeHandler();
+          this.syncLog();
       },
       isValidScreen(){
         return this.root.offsetHeight > NORMAL_PAGE_MINI_HEIGHT
@@ -22,24 +23,54 @@ if(typeof window.MutationObserver!=='undefined'){
       _changeHandler(e){
         this._tick && clearTimeout(this._tick);
         this._tick = setTimeout(()=>{
-          !this.isValidScreen() && this._detectedHandler()
+          !this.isValidScreen() && this._detectedHandler(this.getClientInfo())
         }, WHITE_STATUS_DURATION)
       },
       detect(callback){
         this._detectedHandler = callback;
-      }
-    }
-
-    WhitescreenDetect.init(rootElement, options);
-
-    WhitescreenDetect.detect(function(){
+      },
+      getClientInfo(){
         let data = {
           ua: navigator.userAgent,
           html: this.root.innerHTML,
           url: location.href
         }
+        //如果因为断网
+        if(navigator.onLine===false){
+          data.offline = true;
+        }
+        return data;
+      },
+      saveLogLocal(data){
+        let last = localStorage.getItem(STORAGE_KEY);
+        last = last ? JSON.parse(last) : [];
+        last.push(data);
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(last))
+      },
+      syncLog(){
+        let last = localStorage.getItem(STORAGE_KEY);
+
+        if(last){
+          this.report(last);
+          localStorage.removeItem(STORAGE_KEY);
+        }
+      },
+      report(jsonStr){
+        console.log('report', jsonStr)
+      }
+    };
+    let onDetected=(data)=>{
+      if(!data) return;
+      if(navigator.onLine){
         //上报数据
-        console.log(data)
-    })
+        WhitescreenDetect.report(JSON.stringify(data));
+
+      }else{
+        WhitescreenDetect.saveLogLocal(data);
+      }
+    }
+    WhitescreenDetect.init(rootElement, options);
+
+    WhitescreenDetect.detect(onDetected);
   })
 }
